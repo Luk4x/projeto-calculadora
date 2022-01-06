54c804bdae5e076e73a9de6a317e64db27866212
Luk4x <luk4xm4ci3l@gmail.com>
2022-01-06 13:42:32 -0300

    key mapping

--- script.js
+++ script.js
@@ -1,3 +1,30 @@
+document.addEventListener('keypress', keyboardLis)
+document.addEventListener('keydown', backspaceLis) // fresco
+
+function keyboardLis(keyp) {
+    if ((keyp.key>=0 && keyp.key<=9) || keyp.key=='*' || keyp.key=='/' || keyp.key=='-' || keyp.key=='+' || keyp.key==',' || keyp.key=='.') {
+        if (keyp.key==',') {
+            document.getElementById('displayCalc').value += '.'
+        } else {
+            document.getElementById('displayCalc').value += keyp.key
+        }
+    } else if (keyp.key=='%') {
+        percentCalc()
+    } else if (keyp.key=='Enter') {
+        calculate()
+    } else if (keyp.key=='Delete') {
+        displayClear()
+    } else if (keyp.key=='?') {
+        invertSign()
+    }
+}
+
+function backspaceLis(keyp) {
+    if (keyp.keyCode=='8') {
+        displayClear()
+    }
+}
+
 function insertValues(value) {
     document.getElementById('displayCalc').value += value
 }
